ad946c94c6bfe45c137468294f849537d593bb40
Juan Vargas Soto <jvargass@login1.isaac.utk.edu>
2023-10-04 03:41:29 -0400

    initial commit of FITS rds objects for w2 day and w3 day. Updated fit_ctmm_ud script.

--- fit_ctmm_ud.R
+++ fit_ctmm_ud.R
@@ -11,25 +11,48 @@ library(ctmm)
 library(sp)
 
 # Prepare parallel workers
+length(availableWorkers())
 cl <- makeCluster(length(availableWorkers()))
-registerDoParallel(cl)
+
+
+
+#registerDoParallel(cl)
 
 # read in data
 telemetries <- readRDS("data/telemetries_w1_day.rds")
 GUESS <- readRDS("data/GUESS_w1_day.rds")
-pond <- sf::read_sf("data/polygon_pond.shp")
-SP <- SpatialPolygons(list(Polygons(list(Polygon(sf::st_coordinates(pond$geometry)[,1:2])), 1)), proj4string = CRS("+proj=tmerc"))
-
-# create CTMM objects in parallel
-CTMMs <- foreach(i = seq_along(telemetries), .packages = "ctmm") %dopar% {
-  # Fit the CTMM models. 1 took about 15 minutes on a single core.
-  ctmm.select(telemetries[[i]], GUESS[[i]])
+SP <- raster::shapefile("data/polygon_pond.shp")
+#SP <- SpatialPolygons(list(Polygons(list(Polygon(sf::st_coordinates(pond$geometry)[,1:2])), 1)), proj4string = CRS("+proj=tmerc"))
+# Define functions to run in parallel, with intermediate output
+parctmm<-function(x) {
+    out<-ctmm::ctmm.select(telemetries[[x]],GUESS[[x]])
+    saveRDS(out,paste0("outputs/FIT_",x,".rds"))
+    out
 }
 
+parakde <- function(x) {
+    out<-ctmm::akde(telemetries[[x]],CTMMs[[x]],SP=SP, grid = list(dr = c(0.5,0.5), align.to.origin = T))
+    saveRDS(out,paste0("outputs/UD_",x,".rds"))
+    out
+}
+
+# Send the functions and variables to all workers
+clusterExport(cl = cl, c("telemetries", "GUESS", "SP", "parctmm", "parakde"))
+
+# Run in parallel
+CTMMs <- clusterApplyLB(cl = cl, seq_along(telemetries), parctmm)
+saveRDS(CTMMs, "w1_day_FITS.rds")
+UD <- clusterApplyLB(cl = cl, seq_along(telemetries), parakde)
+saveRDS(UD, "w1_day_UDS.rds")
+# create CTMM objects in parallel
+#CTMMs <- foreach(i = seq_along(telemetries), .packages = "ctmm") %dopar% {
+  # Fit the CTMM models. 1 took about 15 minutes on a single core.
+#  ctmm.select(telemetries[[i]], GUESS[[i]])
+#}
+
 # Estimate the UDs
-UD <- akde(telemetries, CTMMs, SP = SP, grid = list(dr = c(0.5,0.5)))
+
+#UD <- akde(telemetries, CTMMs, SP = SP, grid = list(dr = c(0.5,0.5), align.to.origin=TRUE))
 
 # Export the CTMM and UDs
-saveRDS(UD, "w1_day_UDS.rds")
-saveRDS(CTMMs, "w1_day_FITS.rds")
 
